845651065e9c59b7af04589e1f60c49df20b2879
0xlaurens <lauwiv@gmail.com>
2024-03-20 15:18:38 +0100

    Domain -> Pipelinephases

--- Domain/Models/PipelinePhase/TestPhase.cs
+++ Domain/Models/PipelinePhase/TestPhase.cs
@@ -2,7 +2,17 @@ using Domain.Interfaces;
 
 namespace Domain.Models.PipelinePhase;
 
-public class TestPhase: IPipelinePhase
+public class TestPhase(string name) : BasePipelinePhase(name)
 {
-    
+    public override void Execute()
+    {
+        Console.WriteLine($"Running the {this}" );
+        Console.WriteLine($"{this} has {Children.Count} child phases: {string.Join(", ", Children)}");
+        
+        foreach (var phase in Children)
+        {
+            phase.Execute(); 
+        }
+    }
+
 }
