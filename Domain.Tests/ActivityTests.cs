b1966007627043d82b39fdb34988b62175c12e53
Bas van Turnhout <ba.vanturnhout@student.avans.nl>
2024-03-24 21:40:30 +0100

    Fix activity tests

--- Domain.Tests/ActivityTests.cs
+++ Domain.Tests/ActivityTests.cs
@@ -33,38 +33,38 @@ public class ActivityTests
         Assert.That(activity.GetState().GetType(), Is.EqualTo(typeof(Domain.Activity.ActivityDoingState)));
     }
 
-    // // integration test: F11 Check if user switches Activity
-    // [Domain.Tests]
-    // public void Check_User_has_Multiple_Activities()
-    // {
-    //     var sw = new StringWriter();
-    //     Console.SetOut(sw);
-    //
-    //     var user = new Developer("developer", "email@developer.nl");
-    //     var backlogItem = new BacklogItem("Domain.Tests backlog");
-    //     var activity = new Activity("test activity", user, backlogItem);
-    //     backlogItem.AddActivity(activity);
-    //
-    //     var activity2 = new Activity("test activity2", user, backlogItem);
-    //
-    //     backlogItem.AddActivity(activity2);
-    //     activity.Subscribe(new EmailNotificationSubscriber<IActivityContext>());
-    //     activity2.Subscribe(new EmailNotificationSubscriber<IActivityContext>());
-    //
-    //     activity.SetState(new Domain.Activity.ActivityTodoState(activity));
-    //     activity.GetState().SetDoing();
-    //     activity2.SetState(new Domain.Activity.ActivityTodoState(activity2));
-    //     activity2.GetState().SetDoing();
-    //
-    //     // Assert
-    //     var expectedOutput =
-    //         $"Sending notification to scrum master Sending email notification: {activity} Sending email notification: {activity} Sending email notification: {activity} Sending email notification: {activity}";
-    //     
-    //     Assert.Multiple(() =>
-    //     {
-    //         Assert.That(sw.ToString().Replace(Environment.NewLine, " "), Is.EqualTo(expectedOutput));
-    //         Assert.That(activity.GetState().GetType(), Is.EqualTo(typeof(Domain.Activity.ActivityDoingState)));
-    //         Assert.That(activity2.GetState().GetType(), Is.EqualTo(typeof(Domain.Activity.ActivityDoingState)));
-    //     });
-    // }
+    // integration test: F11 Check if user switches Activity
+    [Test]
+    public void Check_User_has_Multiple_Activities()
+    {
+        var sw = new StringWriter();
+        Console.SetOut(sw);
+    
+        var user = new Developer("developer", "email@developer.nl");
+        var backlogItem = new BacklogItem("Domain.Tests backlog");
+        var activity = new Activity("test activity", user, backlogItem);
+        backlogItem.AddActivity(activity);
+    
+        var activity2 = new Activity("test activity2", user, backlogItem);
+    
+        backlogItem.AddActivity(activity2);
+        activity.Subscribe(new EmailNotificationSubscriber<IActivityContext>());
+        activity2.Subscribe(new EmailNotificationSubscriber<IActivityContext>());
+    
+        activity.SetState(new Domain.Activity.ActivityTodoState(activity));
+        activity.GetState().SetDoing();
+        activity2.SetState(new Domain.Activity.ActivityTodoState(activity2));
+        activity2.GetState().SetDoing();
+    
+        // Assert
+        var expectedOutput =
+            $"Sending notification to scrum master Sending email notification: Activity in state: Todo Sending email notification: {activity} Sending email notification: Activity in state: Todo Sending email notification: {activity} ";
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(sw.ToString().Replace(Environment.NewLine, " "), Is.EqualTo(expectedOutput));
+            Assert.That(activity.GetState().GetType(), Is.EqualTo(typeof(Domain.Activity.ActivityDoingState)));
+             Assert.That(activity2.GetState().GetType(), Is.EqualTo(typeof(Domain.Activity.ActivityDoingState)));
+         });
+     }
 }
